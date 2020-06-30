435167d669a810d1814067bb261ffc497aa871f3
Luis Valgoi <luis.valgoi@sap.com>
2020-06-30 10:59:28 -0300

    feat: set mockserver

---
+++ src/server/data/GET_POKEMONS.js
@@ -0,0 +1,199 @@
+module.exports = {
+  "data": {
+    "query": {
+      "_pokemonsLEhzg": [
+        {
+          "id": "UG9rZW1vbjowMDE=",
+          "number": "001",
+          "name": "Bulbasaur",
+          "image": "https://img.pokemondb.net/artwork/bulbasaur.jpg",
+          "types": [
+            "Grass",
+            "Poison"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMDI=",
+          "number": "002",
+          "name": "Ivysaur",
+          "image": "https://img.pokemondb.net/artwork/ivysaur.jpg",
+          "types": [
+            "Grass",
+            "Poison"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMDM=",
+          "number": "003",
+          "name": "Venusaur",
+          "image": "https://img.pokemondb.net/artwork/venusaur.jpg",
+          "types": [
+            "Grass",
+            "Poison"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMDQ=",
+          "number": "004",
+          "name": "Charmander",
+          "image": "https://img.pokemondb.net/artwork/charmander.jpg",
+          "types": [
+            "Fire"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMDU=",
+          "number": "005",
+          "name": "Charmeleon",
+          "image": "https://img.pokemondb.net/artwork/charmeleon.jpg",
+          "types": [
+            "Fire"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMDY=",
+          "number": "006",
+          "name": "Charizard",
+          "image": "https://img.pokemondb.net/artwork/charizard.jpg",
+          "types": [
+            "Fire",
+            "Flying"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMDc=",
+          "number": "007",
+          "name": "Squirtle",
+          "image": "https://img.pokemondb.net/artwork/squirtle.jpg",
+          "types": [
+            "Water"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMDg=",
+          "number": "008",
+          "name": "Wartortle",
+          "image": "https://img.pokemondb.net/artwork/wartortle.jpg",
+          "types": [
+            "Water"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMDk=",
+          "number": "009",
+          "name": "Blastoise",
+          "image": "https://img.pokemondb.net/artwork/blastoise.jpg",
+          "types": [
+            "Water"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTA=",
+          "number": "010",
+          "name": "Caterpie",
+          "image": "https://img.pokemondb.net/artwork/caterpie.jpg",
+          "types": [
+            "Bug"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTE=",
+          "number": "011",
+          "name": "Metapod",
+          "image": "https://img.pokemondb.net/artwork/metapod.jpg",
+          "types": [
+            "Bug"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTI=",
+          "number": "012",
+          "name": "Butterfree",
+          "image": "https://img.pokemondb.net/artwork/butterfree.jpg",
+          "types": [
+            "Bug",
+            "Flying"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTM=",
+          "number": "013",
+          "name": "Weedle",
+          "image": "https://img.pokemondb.net/artwork/weedle.jpg",
+          "types": [
+            "Bug",
+            "Poison"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTQ=",
+          "number": "014",
+          "name": "Kakuna",
+          "image": "https://img.pokemondb.net/artwork/kakuna.jpg",
+          "types": [
+            "Bug",
+            "Poison"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTU=",
+          "number": "015",
+          "name": "Beedrill",
+          "image": "https://img.pokemondb.net/artwork/beedrill.jpg",
+          "types": [
+            "Bug",
+            "Poison"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTY=",
+          "number": "016",
+          "name": "Pidgey",
+          "image": "https://img.pokemondb.net/artwork/pidgey.jpg",
+          "types": [
+            "Normal",
+            "Flying"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTc=",
+          "number": "017",
+          "name": "Pidgeotto",
+          "image": "https://img.pokemondb.net/artwork/pidgeotto.jpg",
+          "types": [
+            "Normal",
+            "Flying"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTg=",
+          "number": "018",
+          "name": "Pidgeot",
+          "image": "https://img.pokemondb.net/artwork/pidgeot.jpg",
+          "types": [
+            "Normal",
+            "Flying"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMTk=",
+          "number": "019",
+          "name": "Rattata",
+          "image": "https://img.pokemondb.net/artwork/rattata.jpg",
+          "types": [
+            "Normal"
+          ]
+        },
+        {
+          "id": "UG9rZW1vbjowMjA=",
+          "number": "020",
+          "name": "Raticate",
+          "image": "https://img.pokemondb.net/artwork/raticate.jpg",
+          "types": [
+            "Normal"
+          ]
+        }
+      ]
+    }
+  }
+};
---
+++ src/server/mockserver.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+
+module.exports = () => ({
+  GET_POKEMONS: require('./data/GET_POKEMONS'),
+  GET_POKEMON_BY_ID: require('./data/GET_POKEMON_BY_ID'),
+  GET_POKEMON_BY_NAME: require('./data/GET_POKEMON_BY_NAME'),
+});
