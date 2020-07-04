5957ae51660beb45b3458f55332e233a5dcac983
Luis Valgoi <luis.valgoi@sap.com>
2020-07-04 20:14:58 -0300

    fix: goback info

--- src/pages/Edit/PokemonEdit.js
+++ src/pages/Edit/PokemonEdit.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import { useActions } from '../../hooks/useActions';
 
 import Button from 'react-bootstrap/Button';
 import Spinner from '../../components/Spinner/Spinner';
 import PokemonForm from './PokemonForm';
 
-import URLProvider from '../../util/URLProvider';
 import BrowserURL from '../../util/BrowserURL';
 import { hasData } from '../../util/Verficator';
 
 import ActionEdit from '../../redux/Pokemon/actions/Edit';
 
 const PokemonEdit = ({ match }) => {
+  const history = useHistory();
   const pokemon = useSelector(state => state.pokemon.edit.item);
 
   const [onPokemonLoad] = useActions([(id) => ActionEdit.ON_ITEM_LOAD_REQUEST(id)], []);
@@ -28,11 +28,9 @@ const PokemonEdit = ({ match }) => {
   const getContent = () => {
     return (
       <PokemonForm data={pokemon.data} onSubmit={(values) => alert(JSON.stringify(values))}>
-        <Link to={URLProvider.replace(BrowserURL.DETAIL, pokemon.data.id)}>
-          <Button className='mr-2' variant='link' disabled={false}>Go Back</Button>
-        </Link>
+        <Button onClick={() => history.goBack()} className='mr-2' variant='link' disabled={false}>Go Back</Button>
         <Button type="submit" variant='primary' disabled={false}>Save</Button>
-      </PokemonForm>
+      </PokemonForm >
     );
   };
 
