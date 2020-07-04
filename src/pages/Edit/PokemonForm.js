dc24156c3643455f3e8382b2f2ab17413308aab3
Luis Valgoi <luis.valgoi@sap.com>
2020-07-04 19:17:56 -0300

    refactor: added partially form pokemon

--- src/pages/Edit/PokemonForm.js
+++ src/pages/Edit/PokemonForm.js
@@ -1,147 +1,96 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, getIn } from 'formik';
 import * as yup from 'yup';
 
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 
-const schema = yup.object({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  username: yup.string().required(),
-  city: yup.string().required(),
-  state: yup.string().required(),
-  zip: yup.string().required(),
-  terms: yup.bool().required(),
-});
-
 const PokemonForm = ({ data }) => {
+  const schema = yup.object().shape({
+    name: yup.string().required(),
+    classification: yup.string().required(),
+    weight: yup.object().shape({
+      minimum: yup.string().max(5, 'Too long').required(),
+      maximum: yup.string().max(5, 'Too long').required(),
+    }),
+    height: yup.object().shape({
+      minimum: yup.string().max(5, 'Too long').required(),
+      maximum: yup.string().max(5, 'Too long').required(),
+    }),
+    fleeRate: yup.string().max(4, 'Too long').required(),
+    maxCP: yup.string().max(4, 'Too long').required(),
+    maxHP: yup.string().max(4, 'Too long').required(),
+    image: yup.string().required()
+  });
+
   return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={console.log}
-      initialValues={{
-        firstName: 'Mark',
-        lastName: 'Otto',
-      }}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        values,
-        touched,
-        isValid,
-        errors,
-      }) => (
-          <Form noValidate onSubmit={handleSubmit}>
-            <Form.Row>
-              <Form.Group as={Col} md="4" controlId="validationFormik01">
-                <Form.Label>First name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="firstName"
-                  value={values.firstName}
-                  onChange={handleChange}
-                  isValid={touched.firstName && !errors.firstName}
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="validationFormik02">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="lastName"
-                  value={values.lastName}
-                  onChange={handleChange}
-                  isValid={touched.lastName && !errors.lastName}
-                />
-
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="validationFormikUsername">
-                <Form.Label>Username</Form.Label>
-                <InputGroup>
-                  <InputGroup.Prepend>
-                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control
-                    type="text"
-                    placeholder="Username"
-                    aria-describedby="inputGroupPrepend"
-                    name="username"
-                    value={values.username}
-                    onChange={handleChange}
-                    isInvalid={!!errors.username}
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.username}
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} md="6" controlId="validationFormik03">
-                <Form.Label>City</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="City"
-                  name="city"
-                  value={values.city}
-                  onChange={handleChange}
-                  isInvalid={!!errors.city}
-                />
-
-                <Form.Control.Feedback type="invalid">
-                  {errors.city}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" controlId="validationFormik04">
-                <Form.Label>State</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="State"
-                  name="state"
-                  value={values.state}
-                  onChange={handleChange}
-                  isInvalid={!!errors.state}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.state}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" controlId="validationFormik05">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Zip"
-                  name="zip"
-                  value={values.zip}
-                  onChange={handleChange}
-                  isInvalid={!!errors.zip}
-                />
-
-                <Form.Control.Feedback type="invalid">
-                  {errors.zip}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Form.Row>
-            <Form.Group>
-              <Form.Check
-                required
-                name="terms"
-                label="Agree to terms and conditions"
-                onChange={handleChange}
-                isInvalid={!!errors.terms}
-                feedback={errors.terms}
-                id="validationFormik0"
-              />
+    <Formik validationSchema={schema} onSubmit={console.log} initialValues={data}>
+      {({ handleSubmit, handleChange, isSubmitting, values, touched, errors }) => (
+        <Form onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} md="6" controlId="nameControl">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" name="name" value={values.name} onChange={handleChange} isInvalid={!!errors.name} isValid={touched.name && !errors.name} />
+              <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
             </Form.Group>
-            <Button type="submit">Submit form</Button>
-          </Form>
-        )}
+
+            <Form.Group as={Col} md="6" controlId="classificationControl">
+              <Form.Label>Classification</Form.Label>
+              <Form.Control type="text" name="classification" value={values.classification} onChange={handleChange} isValid={touched.classification && !errors.classification} isInvalid={!!errors.classification} />
+              <Form.Control.Feedback type="invalid">{errors.classification}</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} md="3" controlId="minimunWeightControl">
+              <Form.Label>Minimum Weight</Form.Label>
+              <Form.Control type="string" name="weight.maximum" value={values.weight.maximum} onChange={handleChange} isValid={getIn(touched, 'weight.maximum') && !getIn(errors, 'weight.maximum')} isInvalid={getIn(errors, 'weight.maximum')} />
+              <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group as={Col} md="3" controlId="maximumWeightControl">
+              <Form.Label>Maximum Weight</Form.Label>
+              <Form.Control type="string" name="weight.minimum" value={values.weight.minimum} onChange={handleChange} isValid={getIn(touched, 'weight.minimum') && !getIn(errors, 'weight.minimum')} isInvalid={getIn(errors, 'weight.minimum')} />
+              <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group as={Col} md="3" controlId="minimunHeightControl">
+              <Form.Label>Minimum Height</Form.Label>
+              <Form.Control type="string" name="height.maximum" value={values.height.maximum} onChange={handleChange} isValid={getIn(touched, 'height.maximum') && !getIn(errors, 'height.maximum')} isInvalid={getIn(errors, 'height.maximum')} />
+              <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group as={Col} md="3" controlId="maximumHeightControl">
+              <Form.Label>Maximum Height</Form.Label>
+              <Form.Control type="string" name="height.minimum" value={values.height.minimum} onChange={handleChange} isValid={getIn(touched, 'height.minimum') && !getIn(errors, 'height.minimum')} isInvalid={getIn(errors, 'height.minimum')} />
+              <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} md="4" controlId="fleeRateControl">
+              <Form.Label>Flee Rate</Form.Label>
+              <Form.Control type="string" name="fleeRate" value={values.fleeRate} onChange={handleChange} isValid={touched.fleeRate && !errors.fleeRate} isInvalid={!!errors.fleeRate} />
+              <Form.Control.Feedback type="invalid">{errors.fleeRate}</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group as={Col} md="4" controlId="maxCPControl">
+              <Form.Label>Maximum CP</Form.Label>
+              <Form.Control type="string" name="maxCP" value={values.maxCP} onChange={handleChange} isValid={touched.maxCP && !errors.maxCP} isInvalid={!!errors.maxCP} />
+              <Form.Control.Feedback type="invalid">{errors.maxCP}</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group as={Col} md="4" controlId="maxHPControl">
+              <Form.Label>Maximum HP</Form.Label>
+              <Form.Control type="string" name="maxHP" value={values.maxHP} onChange={handleChange} isValid={touched.maxHP && !errors.maxHP} isInvalid={!!errors.maxHP} />
+              <Form.Control.Feedback type="invalid">{errors.maxHP}</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+
+          <Button type="submit" disabled={isSubmitting}>Save</Button>
+        </Form>
+      )}
     </Formik>
   );
 }
