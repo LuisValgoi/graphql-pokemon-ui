ff56708c2c85d24ef1d7f30132aaf930f9eb94a0
Luis Valgoi <luis.valgoi@sap.com>
2020-07-05 11:14:21 -0300

    feat: added no items were found indicator for localstorage

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -7,17 +7,19 @@ import Alert from 'react-bootstrap/Alert';
 import CardItem from '../CardItem/CardItem';
 
 const CardList = ({ items, onShowDeleteModal }) => {
+  let activeItemsLength = 0;
   return (
     <>
       <CardColumns>
         {items.map(item => {
           if (item.active && item.shown) {
+            activeItemsLength++;
             return <CardItem key={item.id} item={item} onShowDeleteModal={onShowDeleteModal} />
           }
         })}
       </CardColumns>
 
-      {items && items.length === 0 && (
+      {activeItemsLength === 0 && (
         <Alert className='text-center' variant="secondary">No items were found</Alert>
       )}
     </>
