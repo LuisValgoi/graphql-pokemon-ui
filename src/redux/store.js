b380b35011aac8a439daf58f6a4a9a8353b46cfa
Luis Valgoi <luis.valgoi@sap.com>
2020-07-05 11:03:53 -0300

    feat: applied localstorage engine to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,18 @@
 
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { loadState, saveState } from '../util/LocalStorage';
+import throttle from 'lodash/throttle';
 
 import RootReducer from './RootReducer';
 import RootSaga from './RootSaga';
 
+const persistedState = loadState();
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  RootReducer,
-  composeEnhancers(applyMiddleware(...middleware))
-);
+const store = createStore(RootReducer, persistedState, composeEnhancers(applyMiddleware(...middleware)));
 sagaMiddleware.run(RootSaga);
+store.subscribe(throttle(() => { saveState({ ...store.getState() }) }, 1000));
 
 export default store;
