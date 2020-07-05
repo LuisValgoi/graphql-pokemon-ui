9bd5596963c771f89a3f75d4bbd5e1a0dae972f3
Luis Valgoi <luis.valgoi@sap.com>
2020-07-05 20:14:37 -0300

    fix: card list layout

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,28 +1,29 @@
 /* eslint-disable array-callback-return */
 import React from 'react'
 
-import CardColumns from 'react-bootstrap/CardColumns';
 import Alert from 'react-bootstrap/Alert';
 
 import CardItem from '../CardItem/CardItem';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 const CardList = ({ items, onShowDeleteModal }) => {
   let activeItemsLength = 0;
   return (
-    <>
-      <CardColumns>
+    <Container fluid>
+      <Row>
         {items.map(item => {
           if (item.active && item.shown) {
             activeItemsLength++;
             return <CardItem key={item.id} item={item} onShowDeleteModal={onShowDeleteModal} />
           }
         })}
-      </CardColumns>
+      </Row>
 
       {activeItemsLength === 0 && (
         <Alert className='text-center' variant="secondary">No items were found</Alert>
       )}
-    </>
+    </Container>
   );
 };
 
