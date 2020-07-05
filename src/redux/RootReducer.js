33a598d310842e0583f43ef90e2413f37ba5fc4c
Luis Valgoi <luis.valgoi@sap.com>
2020-07-05 11:04:13 -0300

    feat: added persistence forgot reducer

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -6,12 +6,14 @@ import ModalReducer from './ViewSettings/reducers/Modal';
 import PokemonListReducer from './Pokemon/reducers/List';
 import PokemonDetailReducer from './Pokemon/reducers/Detail';
 import PokemonEditReducer from './Pokemon/reducers/Edit';
+import PokemonPersistenceReducer from './Pokemon/reducers/Persistence';
 
 export default combineReducers({
   pokemon: combineReducers({
     list: PokemonListReducer,
     detail: PokemonDetailReducer,
-    edit: PokemonEditReducer
+    edit: PokemonEditReducer,
+    persistence: PokemonPersistenceReducer
   }),
   viewSettings: combineReducers({
     notification: NotificationReducer,
