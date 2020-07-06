ac92a32dd8060d291a42147c0d9bc45ea4c6c931
Luis Valgoi <luis.valgoi@sap.com>
2020-07-06 16:05:51 -0300

    test: added one more tc to toolbar

--- src/components/Toolbar/Toolbar.test.js
+++ src/components/Toolbar/Toolbar.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Toolbar from './Toolbar';
 
@@ -17,3 +17,11 @@ test('should have placeholder w/ the text: "Search by a pokemon name"', () => {
   expect(getByTestId('toolbar-input')).toHaveAttribute('placeholder', 'Search by a pokemon name');
 });
 
+test('should set variable through IoC to "text"', () => {
+  let variable = '';
+  const onSearch = (e) => variable = e;
+  const utils = render(<Toolbar onSearch={onSearch} />);
+  const input = utils.getByTestId('toolbar-input');
+  fireEvent.input(input, { target: { value: 'text' } });
+  expect(input.value).toBe(variable);
+});
