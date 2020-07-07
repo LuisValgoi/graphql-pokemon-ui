7dfb6c5afc6a733b582529d89ad53ad94cb5059a
Luis Valgoi <luis.valgoi@sap.com>
2020-07-07 14:26:58 -0300

    test: finished tests for shell

--- src/components/Shell/Shell.test.js
+++ src/components/Shell/Shell.test.js
@@ -8,7 +8,7 @@ import PokemonDetailReducer from '../../redux/Pokemon/reducers/Detail';
 let INITIAL_STATE = {};
 
 beforeEach(() => {
-  INITIAL_STATE = { pokemon: { detail: { item: { data: { id: 1 } } } } };
+  INITIAL_STATE = { pokemon: { detail: { item: { data: { id: 'UG9rZW1vbjowMDM=' } } } } };
   render(<Shell title='title' />, { reducer: PokemonDetailReducer, mockStore: INITIAL_STATE });
 });
 
@@ -25,10 +25,10 @@ test('should have a little padding', () => {
   expect(shell).toHaveClass('p-4');
 });
 
-test('should not be shown at home', () => {
+test('home link should not be shown at home', () => {
   const shell = screen.getByTestId('shell-wrapper');
   const homeCol = screen.getByTestId('shell-home-col');
-  const homeLink = screen.queryByTestId('shell-home');
+  const homeLink = screen.queryByTestId('shell-home-link');
 
   expect(shell).toBeInTheDocument();
   expect(shell).toContainElement(homeCol);
@@ -36,13 +36,35 @@ test('should not be shown at home', () => {
 });
 
 test('home link should be shown at details page', () => {
-  render(<Shell title='title' />, { reducer: PokemonDetailReducer, mockStore: INITIAL_STATE, route: '/pokemon/1' });
+  render(<Shell title='title' />, { reducer: PokemonDetailReducer, mockStore: INITIAL_STATE, route: '/pokemon/UG9rZW1vbjowMDM=' });
 
   const homeCol = screen.queryAllByTestId('shell-home-col')[1];
-  const homeLink = screen.getByTestId('shell-home');
+  const homeLink = screen.getByTestId('shell-home-link');
 
   expect(homeCol).toBeInTheDocument();
   expect(homeLink).toBeInTheDocument();
   expect(homeCol).toContainElement(homeLink);
   expect(homeLink).toHaveTextContent('Home');
+});
+
+test('edit button should not be shown at home', () => {
+  const shell = screen.getByTestId('shell-wrapper');
+  const editCol = screen.getByTestId('shell-edit-col');
+  const editButton = screen.queryByTestId('shell-edit-button');
+
+  expect(shell).toBeInTheDocument();
+  expect(shell).toContainElement(editCol);
+  expect(editCol).not.toContainElement(editButton);
+});
+
+
+test('edit button should be shown at detail', () => {
+  render(<Shell title='title' />, { reducer: PokemonDetailReducer, mockStore: INITIAL_STATE, route: '/pokemon/UG9rZW1vbjowMDM=' });
+  const editCol = screen.queryAllByTestId('shell-edit-col')[1];
+  const editButton = screen.getByTestId('shell-edit-button');
+
+  expect(editCol).toBeInTheDocument();
+  expect(editButton).toBeInTheDocument();
+  expect(editCol).toContainElement(editButton);
+  expect(editButton).toHaveTextContent('Edit');
 });
